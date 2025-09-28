fbeb644fa47bfb68bbcf3e1adf417e59de158bf2
Atos Roberto <atos.roberto@gmail.com>
2025-09-28 15:48:46 -0300

    patch mapping

---
+++ src/main/java/br/com/atos/gestao_alugueis/dtos/AtualizarPagamentoDto.java
@@ -0,0 +1,14 @@
+package br.com.atos.gestao_alugueis.dtos;
+
+public class AtualizarPagamentoDto {
+    private boolean pago;
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public void setPago(boolean pago) {
+        this.pago = pago;
+    }
+}
+
